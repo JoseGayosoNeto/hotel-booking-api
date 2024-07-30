dbe40520d3d7f82eac1af9ecf910ae3fcbe1e858
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-07-30 09:14:02 -0300

    fix: update users admin to adjust creation and change form in admin page

--- user/admin.py
+++ user/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from user.forms import (
     CustomUserCreationForm,
@@ -16,26 +17,62 @@ from user.models import (
 
 
 @admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(BaseUserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
+    model = CustomUser
     list_display = ('id', 'email', 'is_staff', 'is_active', 'created_at', 'updated_at',)
     list_filter = ('email', 'is_active', 'is_staff',)
     search_fields = ('email', 'first_name', 'last_name', 'phone_number', 'city', 'country',)
     ordering = ("email",)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone_number',
+                                      'address', 'postal_code', 'city', 'country',
+                                      'date_of_birthday', 'gender',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'phone_number', 'address',
+                       'postal_code', 'city', 'country', 'date_of_birthday', 'gender',
+                       'password1', 'password2'),
+        }),
+    )
 
 @admin.register(Employee)
 class EmployeeAdmin(CustomUserAdmin):
     add_form = EmployeeCreationForm
     form = EmployeeChangeForm
+    model = Employee
     list_display = CustomUserAdmin.list_display + ('role', 'hotel', 'salary',)
     list_filter = ('email', 'is_active',)
     search_fields = CustomUserAdmin.search_fields + ('role__name', 'hotel__name', 'salary',)
+    fieldsets = CustomUserAdmin.fieldsets + (
+        ('Job Info', {'fields': ('role', 'hotel', 'salary',)}),
+    )
+    add_fieldsets = CustomUserAdmin.add_fieldsets + (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('role', 'hotel', 'salary'),
+        }),
+    )
 
 @admin.register(Guest)
 class GuestAdmin(CustomUserAdmin):
     add_form = GuestCreationForm
     form = GuestChangeForm
+    model = Guest
     list_display = CustomUserAdmin.list_display
     list_filter = ('email', 'is_active',)
-    search_fields = ('email', 'hotel', 'role',)
+    search_fields = ('email', 'passport_number',)
+    fieldsets = CustomUserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('passport_number',)}),
+    )
+    add_fieldsets = CustomUserAdmin.add_fieldsets + (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('passport_number',),
+        }),
+    )
