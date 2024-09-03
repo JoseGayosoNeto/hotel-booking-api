24a70e9e0cedb2e51eb60fb07ac593a8356adcd1
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-09-03 11:40:34 -0300

    fix: adjust drf-spectacular error docs using drf-standardized-errors

--- roles/views.py
+++ roles/views.py
@@ -1,3 +1,4 @@
+from drf_standardized_errors.openapi import AutoSchema
 from rest_framework import generics
 from roles.models import Role
 from roles.serializers import RoleSerializer
@@ -5,6 +6,7 @@ from roles.serializers import RoleSerializer
 class RoleListCreateView(generics.ListCreateAPIView):
     queryset = Role.objects.all()
     serializer_class = RoleSerializer
+    schema = AutoSchema()
 
     def get_queryset(self):
         queryset = super().get_queryset()
